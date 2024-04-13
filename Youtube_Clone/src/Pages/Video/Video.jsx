ff60e8aede5845bc7d7c761a5ff73ceaf2208ef3
SomneelMaity <somneel.maity@gmail.com>
2024-04-13 20:38:24 +0530

    Play Video User Comment Area

--- Youtube_Clone/src/Pages/Video/Video.jsx
+++ Youtube_Clone/src/Pages/Video/Video.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./Video.css";
 import PlayVideo from "../../Components/PlayVideo/PlayVideo";
 import Recommended from "../../Components/Recommended/Recommended";
+import { useParams } from "react-router-dom";
 const Video = () => {
-  return <div className="play-container">
-    <PlayVideo />
-    <Recommended />
-  </div>;
+  const { videoId, categoryId } = useParams();
+  return (
+    <div className="play-container">
+      <PlayVideo videoId={videoId} />
+      <Recommended />
+    </div>
+  );
 };
 
 export default Video;
